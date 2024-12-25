4289e1117193d8b2de01b6245ec22aef9f35c1c3
Edson Victor da cruz Menezes <91231848+cruzmenezes@users.noreply.github.com>
2024-12-25 18:13:25 -0300

    operadores matematicos

---
+++ day-2/day-2.R
@@ -0,0 +1,11 @@
+# operadores matematicos
+
+num1 <- 10 + 5
+num2 <- 10 - 5
+num3 <- 10 * 5
+num4 <- 10 / 5
+
+print(paste("o resultado do numero é", num1)) 
+print(paste("o resultado do numero é", num2)) 
+print(paste("o resultado do numero é", num3)) 
+print(paste("o resultado do numero é", num4)) 
